3f59efcf76071bd322f0a9269c809ebe05be4227
ryusei1212 <black245k@gmail.com>
2023-02-22 11:23:09 +0900

    いいねといいね解除ができるように変更

--- app/controllers/users/likes_controller.rb
+++ app/controllers/users/likes_controller.rb
@@ -1,8 +1,13 @@
 class Users::LikesController < Users::ApplicationController
 
   def create
+    Like.create!(user_id: params[:user_id], post_id: params[:post_id])
+    redirect_to root_path
   end
 
   def destroy
+    like_post = current_user.likes.find_by(post_id: params[:post_id])
+    like_post.destroy!
+    redirect_to root_path
   end
 end
