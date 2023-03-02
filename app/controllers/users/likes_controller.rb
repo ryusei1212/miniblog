a4b284ffd7e4f041342247adb121d3c2b02725cf
ryusei1212 <black245k@gmail.com>
2023-03-02 10:23:20 +0900

    いいねようのテストを追加

--- app/controllers/users/likes_controller.rb
+++ app/controllers/users/likes_controller.rb
@@ -1,16 +1,17 @@
 class Users::LikesController < Users::ApplicationController
   def index
-    @liked_post = Post.find(params[:post_id])
+    post = Post.find(params[:post_id])
+    @liked_users = post.liked_users
   end
 
   def create
     current_user.likes.create!(post_id: params[:post_id])
-    redirect_to root_path
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     liked_post = current_user.likes.find_by(post_id: params[:post_id])
     liked_post.destroy!
-    redirect_to root_path
+    redirect_back(fallback_location: root_path)
   end
 end
