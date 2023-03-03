41585902f0f3095a252f9b178982628ec339cb87
ryusei1212 <black245k@gmail.com>
2023-03-03 18:16:25 +0900

    コメント登録機能のスペックを作成

--- spec/system/users/posts/comments_spec.rb
+++ spec/system/users/posts/comments_spec.rb
@@ -4,6 +4,23 @@ RSpec.describe 'コメント機能', type: :system do
   let(:user) { create(:user) }
   let(:post) { create(:post, user:) }
 
+  describe 'コメント登録機能' do
+    let(:post) { create(:post, content: 'こんにちは', user:) }
+
+    it 'コメントを登録できる' do
+      sign_in user
+      visit post_path(post)
+      expect(page).to have_css '#new_comment'
+
+      fill_in 'comment[content]', with: 'いい投稿ですね'
+
+      expect do
+        click_on 'コメントする'
+        expect(page).to have_content 'コメントしました'
+      end.to change(Comment, :count).by(1)
+    end
+  end
+
   describe 'コメント削除機能' do
     let!(:comment) { create(:comment, user:, post:, content: 'おはよう') }
 
