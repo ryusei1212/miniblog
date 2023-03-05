3abd232ff4020756e618247c2c9333a9f57e086c
ryusei1212 <black245k@gmail.com>
2023-03-05 18:26:58 +0900

    自分の投稿にコメントした時はメールが送信されないようにした

--- spec/mailers/previews/comment_preview.rb
+++
@@ -1,3 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/comment
-class CommentPreview < ActionMailer::Preview
-end
--- spec/system/users/posts/comments_spec.rb
+++ spec/system/users/posts/comments_spec.rb
@@ -2,27 +2,54 @@ require 'rails_helper'
 
 RSpec.describe 'コメント機能', type: :system do
   let(:user) { create(:user) }
-  let(:post) { create(:post, user:) }
+  let(:post) { create(:post) }
 
   describe 'コメント登録機能' do
-    let(:post) { create(:post, content: 'こんにちは', user:) }
+    RSpec::Matchers.define_negated_matcher :not_change, :change
 
-    it 'コメントを登録できる' do
-      sign_in user
-      visit post_path(post)
-      expect(page).to have_css '#new_comment'
+    context '自分の投稿にコメントした時' do
+      let(:my_post) { create(:post, content: 'こんにちは', user:) }
 
-      fill_in 'comment[content]', with: 'いい投稿ですね'
+      it 'コメントは登録できるがメールは送信されない' do
+        sign_in user
+        visit post_path(my_post)
+        expect(page).to have_content 'こんにちは'
+        expect(page).to have_css '#new_comment'
 
-      expect do
-        click_on 'コメントする'
-        expect(page).to have_content 'コメントしました'
-      end.to change(Comment, :count).by(1)
+        fill_in 'comment[content]', with: '我ながら良い出来だ'
+
+        expect do
+          click_on 'コメントする'
+          expect(page).to have_content 'コメントしました'
+        end.to change(Comment, :count).by(1).and not_change(ActionMailer::Base.deliveries, :count)
+
+        expect(page).to have_content '我ながら良い出来だ'
+      end
+    end
+
+    context '他人の投稿にコメントした時' do
+      let(:other_user_post) { create(:post, content: 'こんばんは') }
+
+      it 'コメントの登録と投稿したユーザーにメールが送信される' do
+        sign_in user
+        visit post_path(other_user_post)
+        expect(page).to have_content 'こんばんは'
+        expect(page).to have_css '#new_comment'
+
+        fill_in 'comment[content]', with: 'いい投稿ですね'
+
+        expect do
+          click_on 'コメントする'
+          expect(page).to have_content 'コメントしました'
+        end.to change(Comment, :count).by(1).and change(ActionMailer::Base.deliveries, :count).by(1)
+
+        expect(page).to have_content 'いい投稿ですね'
+      end
     end
   end
 
   describe 'コメント削除機能' do
-    let!(:comment) { create(:comment, user:, post:, content: 'おはよう') }
+    let!(:my_comment) { create(:comment, user:, post:, content: 'おはよう') }
 
     it 'コメントを削除できる' do
       sign_in user
