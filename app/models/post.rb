d41105d07e722a728c39b397a1a2db20a46c8605
ryusei1212 <black245k@gmail.com>
2023-03-16 16:55:22 +0900

    集計用のスコープを作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,6 +10,15 @@ class Post < ApplicationRecord
   validates :image, content_type: %w[image/png image/jpeg], size: { less_than: 5.megabytes }
 
   scope :default_order, -> { order(created_at: :desc, id: :asc) }
+  scope :top_10_daily_likes, -> {
+    joins(:likes)
+      .where(likes: { created_at: Date.yesterday.all_day })
+      .group(:id)
+      .order('count_id DESC')
+      .count(:id)
+      .take(10)
+  }
+
 
   def display_image
     image.variant(resize_to_limit: [100, 100])
